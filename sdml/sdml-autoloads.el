16cef737c2cc793765f8996778e149b7b67fc195
Simon Johnston <johnstonskj@gmail.com>
2024-03-05 16:13:27 -0800

    refactor: name is just sdml

---
+++ sdml/sdml-autoloads.el
@@ -0,0 +1,19 @@
+;;; sdml-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil nil ("flycheck_sdml.el" "sdml.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sdml-autoloads.el ends here
